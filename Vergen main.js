4553caf2eaafb443d7896132d4e0f1a71ad7aba5
S8N <Toastysor@gmail.com>
2022-10-20 06:10:57 +0200

    git checkout

--- Vergen main.js
+++ Vergen main.js
@@ -1,2 +1,6 @@
 var date = new Date();
-document.getElementById("p1").innerHTML = date;
+var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
+var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
+var date_time = current_date+" "+current_time;
+document.getElementById("date").innerHTML = date_time;
+
